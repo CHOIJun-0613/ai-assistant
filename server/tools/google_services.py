d798bc0f56005c5e751d4b0198d7b2d2b6d3749a
Jun CHOI <cjonyou@naver.com>
2025-07-15 15:51:50 +0900

    수정

--- server/tools/google_services.py
+++ server/tools/google_services.py
@@ -2,7 +2,12 @@ import os
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
+import datetime
+import os
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
 from server.core.config import settings
 
 SCOPES = [
